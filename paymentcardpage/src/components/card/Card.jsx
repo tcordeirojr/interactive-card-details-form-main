41130ac0c46052f8dd9a423dab0f30c05248b00e
tcordeirojr <tedcordeirojr@gmail.com>
2025-02-08 16:23:55 -0300

    finalizacao de input do image back card

--- paymentcardpage/src/components/card/Card.jsx
+++ paymentcardpage/src/components/card/Card.jsx
@@ -7,11 +7,12 @@ function Card() {
                 <a  href="#">
                     <img className="imgFront" src="./src/assets/bg-card-front.png" alt="" ></img>
                     <img className="logo" src="./src/assets/card-logo.svg" alt="" />
+                    <input className="numberApresentation" type="number" />
+                    <input className="nameApresentation" type="text" />
                 </a> 
                 <a href="#">
                     <img className="imgBack" src="./src/assets/bg-card-back.png" alt="" />
-                    <input className="numberApresentation" type="number" />
-                    <input className="nameApresentation" type="text" />
+                    <input className="cvcApresentation" type="number" />
                 </a>
                 
 
